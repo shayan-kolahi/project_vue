5eb37dc305afab5b3c80c1c92bf75ba047988e97
Shayan <irantech.poshtibani@gmail.com>
2021-10-23 17:20:24 +0330

    query params

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="Profile">
     <h1>This is an Profile page</h1>
+    <p> {{tex}}</p>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return{
+      tex : "",
+    }
+  },
+  mounted(){
+    this.tex = this.$route.query.shayan
+  }, 
+}
+</script>
